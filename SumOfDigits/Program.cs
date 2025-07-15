9a33ad06c3bf4c9a73f112a2ab4a6f1a438f2ced
Younga <youngajin93@gmail.com>
2025-07-15 20:19:22 +0900

    자릿수더하기 SumOfDigits

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SumOfDigits
+{
+    internal class Program
+    {
+        //자릿수 더하기
+        //https://school.programmers.co.kr/learn/courses/30/lessons/12931
+        static void Main(string[] args)
+        {
+            int num1 = 123;
+            int num2 = 987;
+
+            Console.WriteLine(Solution(num1));
+            Console.WriteLine(Solution(num2));
+        }
+        public static int Solution(int n)
+        {
+            int answer = 0;
+            int currentNumber = n;
+            while (currentNumber != 0)
+            {
+                answer += currentNumber % 10;
+                currentNumber /= 10;
+            }
+            return answer;
+        }
+    }
+}
