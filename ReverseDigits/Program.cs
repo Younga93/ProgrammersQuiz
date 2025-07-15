0126b71c476048ee147fac49a8c07f6bd79ff102
Younga <youngajin93@gmail.com>
2025-07-15 20:35:52 +0900

    자연수 뒤집어 배열로 만들기

---
+++ ReverseDigits/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ReverseDigits
+{
+    internal class Program
+    {
+        //자연수 뒤집어 배열로 만들기
+        //https://school.programmers.co.kr/learn/courses/30/lessons/12932
+        static void Main(string[] args)
+        {
+            int num = 12345;
+            int[] results = Solution(num);
+            foreach (int i in results)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static int[] Solution(long n)
+        {
+            int length = n.ToString().Length;
+            int[] answer = new int[length];
+            long currentNumber = n;
+            for(int i = 0; i < length; i++)
+            {
+                answer[i] = (int)currentNumber % 10;
+                currentNumber /= 10;
+            }
+
+            return answer;
+        }
+    }
+}
